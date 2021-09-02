7eee4e063c7c3d0a3cfd1e9289828e15286d2634
kawsar130 <kawsarahmed130@gmail.com>
2021-09-02 23:43:02 +0600

    Final Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 let searchMessage = document.getElementById('search-message');
+searchMessage.style.fontWeight = "bold";
+const searchResult = document.getElementById('search-result');
 
 const getUserInput = () => {
     const fieldInput = document.getElementById('search-field');
     const userText = fieldInput.value;
     fieldInput.value = '';
-    searchMessage.innerText = '';
+    searchMessage.innerText = 'Be patient, I am Loading!';
+    searchResult.textContent = '';
 
     if (userText == '') {
         searchMessage.innerText = 'Please write something!';
@@ -28,22 +31,26 @@ const displaySearchResult = books => {
     const resultFound = books.numFound;
     searchMessage.innerText = ` We found ${resultFound} books for you.
     `;
-    // console.log(books);
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
+    console.log(books);
     const bookList = books.docs;
     bookList.forEach(eachElement => {
         const bookName = eachElement.title;
         const authorName = eachElement.author_name;
         const publisherName = eachElement.publisher;
         const firstPublished = eachElement.first_publish_year;
+        const coverPhotoId = eachElement.cover_i;
+
+        // cover photo link
+        const url = `https://covers.openlibrary.org/b/id/${coverPhotoId}-M.jpg`;
+
 
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
+        <img src="${url}" height="300px" class="card-img-top w-auto" alt="...">
         <div class="card-body">
-        <h5 class="card-title">${bookName}</h5>
-        <p class="card-text">Author: ${authorName}</p>
+        <h4 class="card-title">${bookName}</h4>
+        <h6 class="card-text">Author: ${authorName}</h6>
         <p class="card-text">Publisher: ${publisherName}</p>
         <p class="card-text">First Published: ${firstPublished}</p>
         </div>
