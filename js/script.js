f64aafb963fefabff5ddda905bdb1a3b6aaa85e5
kawsar130 <kawsarahmed130@gmail.com>
2021-09-02 22:52:18 +0600

    search button working

--- js/script.js
+++ js/script.js
@@ -1,17 +1,53 @@
+let searchMessage = document.getElementById('search-message');
+
 const getUserInput = () => {
     const fieldInput = document.getElementById('search-field');
     const userText = fieldInput.value;
     fieldInput.value = '';
+    searchMessage.innerText = '';
 
-    const url = `https://openlibrary.org/search.json?q=${userText}`;
-
-    // fetching data from the API
-    fetch(url).then(res => res.json()).then(data => searchResult(data));
+    if (userText == '') {
+        searchMessage.innerText = 'Please write something!';
+    }
+    else {
+        const url = `https://openlibrary.org/search.json?q=${userText}`;
+        // fetching data from the API
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data))
+            .catch(error => displayError(error));
+    }
 };
 
-const searchResult = books => {
+const displayError = error => {
+    searchMessage.innerText = error;
+}
+
+
+const displaySearchResult = books => {
     const resultFound = books.numFound;
-    console.log(books);
+    searchMessage.innerText = ` We found ${resultFound} books for you.
+    `;
+    // console.log(books);
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
     const bookList = books.docs;
-    console.log(bookList);
+    bookList.forEach(eachElement => {
+        const bookName = eachElement.title;
+        const authorName = eachElement.author_name;
+        const publisherName = eachElement.publisher;
+        const firstPublished = eachElement.first_publish_year;
+
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card-body">
+        <h5 class="card-title">${bookName}</h5>
+        <p class="card-text">Author: ${authorName}</p>
+        <p class="card-text">Publisher: ${publisherName}</p>
+        <p class="card-text">First Published: ${firstPublished}</p>
+        </div>
+    `;
+        searchResult.appendChild(div);
+    });
 };
